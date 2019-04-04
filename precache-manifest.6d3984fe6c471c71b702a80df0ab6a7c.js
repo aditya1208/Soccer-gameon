5d3c4910cba40a6353f9fead5050eb946b10c58c
aditya_perf <aditya.thallapelly@perficient.com>
2019-04-04 19:15:33 -0400

    Updates

---
+++ precache-manifest.6d3984fe6c471c71b702a80df0ab6a7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef1172c9822fdb4fc93e",
+    "url": "/Soccer-gameon/static/js/runtime~main.77b6edd5.js"
+  },
+  {
+    "revision": "d6029041d81b7f0c608f",
+    "url": "/Soccer-gameon/static/js/main.11fbd83f.chunk.js"
+  },
+  {
+    "revision": "aa21250ce275dbec0a87",
+    "url": "/Soccer-gameon/static/js/2.4abff027.chunk.js"
+  },
+  {
+    "revision": "d6029041d81b7f0c608f",
+    "url": "/Soccer-gameon/static/css/main.28572bd8.chunk.css"
+  },
+  {
+    "revision": "aa21250ce275dbec0a87",
+    "url": "/Soccer-gameon/static/css/2.28f2fc55.chunk.css"
+  },
+  {
+    "revision": "52cffa6f01d8f6a21634b1831edc50c3",
+    "url": "/Soccer-gameon/index.html"
+  }
+];
